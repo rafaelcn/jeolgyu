8e182294263f773a9f6e7d1468be303ad723d7ab
Rafael Campos Nunes <rafaelnunes@engineer.com>
2020-02-06 14:05:35 -0300

    Fixed typo and add documentation
    Signed-off-by: Rafael Campos Nunes <rafaelnunes@engineer.com>

--- level.go
+++ level.go
@@ -3,25 +3,28 @@ package jeolgyu
 type Level int8
 
 const (
-	//
+	// An information message telling you about something. 
 	InfoLevel = iota + 1
-	//
+	// A warning level message. If I'd describe it, it would be a thing to pay
+	// attetion whenever messages of this type appears
 	WarningLevel
-	//
+	// Something wrong happened, hence the error.
+	ErrorLevel
+	// A mesage level so strong you don't want it happening on yout system.
 	PanicLevel
 )
 
-// Returns a string representation of the level 
-func (l *Level) String() string {
+// Returns a string representation of the level
+func (l Level) String() string {
 	s := ""
 
-	switch Level {
+	switch l {
 	case InfoLevel:
 		s = "info"
 	case WarningLevel:
 		s = "warning"
 	case PanicLevel:
-		"panic"
+		s = "panic"
 	}
 
 	return s
