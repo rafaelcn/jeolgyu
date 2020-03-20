23e681d18d77126cfc221c27d2be48a83efa0083
Rafael Campos Nunes <rafaelnunes@engineer.com>
2020-03-20 01:38:38 -0300

    Add a benchmark
    Signed-off-by: Rafael Campos Nunes <rafaelnunes@engineer.com>

---
+++ jeolgyu_bench_test.go
@@ -0,0 +1,55 @@
+package jeolgyu
+
+import "testing"
+
+const (
+	N = 1024
+	N2 = 4096
+	N3 = 65536
+)
+
+// TestSinkFile benchmarks the execution of consecutive log.Info calls
+func BenchmarkSinkFileN(b *testing.B) {
+
+	l, err := New(SinkFile, "/tmp/")
+
+	if err != nil { 
+		b.Fatalf("Couldn't initialize logger. Reason %v", err)
+	}
+
+	b.ResetTimer()
+
+	for i := 0 ; i < N; i++ {
+		l.Info("Such meaningful %d ith message, wow.", i)
+	}
+}
+
+func BenchmarkSinkFileN2(b *testing.B) {
+
+	l, err := New(SinkFile, "/tmp/")
+
+	if err != nil { 
+		b.Fatalf("Couldn't initialize logger. Reason %v", err)
+	}
+
+	b.ResetTimer()
+
+	for i := 0 ; i < N2; i++ {
+		l.Info("Such meaningful %d ith message, wow.", i)
+	}
+}
+
+func BenchmarkSinkFileN3(b *testing.B) {
+
+	l, err := New(SinkFile, "/tmp/")
+
+	if err != nil { 
+		b.Fatalf("Couldn't initialize logger. Reason %v", err)
+	}
+
+	b.ResetTimer()
+
+	for i := 0 ; i < N3; i++ {
+		l.Info("Such meaningful %d ith message, wow.", i)
+	}
+}
