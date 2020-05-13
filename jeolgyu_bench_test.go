74e6db2130616078a67cb6a2fcdd3a6c30a6016a
Rafael Campos Nunes <rafaelnunes@engineer.com>
2020-05-13 17:11:36 -0300

    Adjust the size of N to be more realible
    Signed-off-by: Rafael Campos Nunes <rafaelnunes@engineer.com>

--- jeolgyu_bench_test.go
+++ jeolgyu_bench_test.go
@@ -5,19 +5,9 @@ import (
 	"testing"
 )
 
-const (
-	N  = 1024
-	N2 = 4096
-	N3 = 65536
-)
-
 var (
 	temp = os.TempDir()
 )
-
-// BenchmarkSinkFileN benchmarks the execution of consecutive log.Info calls
-// with size N. The following functions just change the ammount of calls it will
-// make to the logger.
 
 func BenchmarkSinkFileN(b *testing.B) {
 	l, err := New(SinkFile, temp)
@@ -28,37 +18,7 @@ func BenchmarkSinkFileN(b *testing.B) {
 
 	b.ResetTimer()
 
-	for i := 0; i < N; i++ {
-		l.Info("Such meaningful %d ith message, wow.", i)
-	}
-}
-
-func BenchmarkSinkFileN2(b *testing.B) {
-
-	l, err := New(SinkFile, temp)
-
-	if err != nil {
-		b.Fatalf("Couldn't initialize logger. Reason %v", err)
-	}
-
-	b.ResetTimer()
-
-	for i := 0; i < N2; i++ {
-		l.Info("Such meaningful %d ith message, wow.", i)
-	}
-}
-
-func BenchmarkSinkFileN3(b *testing.B) {
-
-	l, err := New(SinkFile, temp)
-
-	if err != nil {
-		b.Fatalf("Couldn't initialize logger. Reason %v", err)
-	}
-
-	b.ResetTimer()
-
-	for i := 0; i < N3; i++ {
+	for i := 0; i < b.N; i++ {
 		l.Info("Such meaningful %d ith message, wow.", i)
 	}
 }
