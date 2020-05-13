e49076f165085975b0255799abe28b0ad826f1d2
Rafael Campos Nunes <rafaelnunes@engineer.com>
2020-05-12 21:53:00 -0300

    Remove trailing whitespaces
    Signed-off-by: Rafael Campos Nunes <rafaelnunes@engineer.com>

--- jeolgyu_bench_test.go
+++ jeolgyu_bench_test.go
@@ -3,7 +3,7 @@ package jeolgyu
 import "testing"
 
 const (
-	N = 1024
+	N  = 1024
 	N2 = 4096
 	N3 = 65536
 )
@@ -13,13 +13,13 @@ func BenchmarkSinkFileN(b *testing.B) {
 
 	l, err := New(SinkFile, "/tmp/")
 
-	if err != nil { 
+	if err != nil {
 		b.Fatalf("Couldn't initialize logger. Reason %v", err)
 	}
 
 	b.ResetTimer()
 
-	for i := 0 ; i < N; i++ {
+	for i := 0; i < N; i++ {
 		l.Info("Such meaningful %d ith message, wow.", i)
 	}
 }
@@ -28,13 +28,13 @@ func BenchmarkSinkFileN2(b *testing.B) {
 
 	l, err := New(SinkFile, "/tmp/")
 
-	if err != nil { 
+	if err != nil {
 		b.Fatalf("Couldn't initialize logger. Reason %v", err)
 	}
 
 	b.ResetTimer()
 
-	for i := 0 ; i < N2; i++ {
+	for i := 0; i < N2; i++ {
 		l.Info("Such meaningful %d ith message, wow.", i)
 	}
 }
@@ -43,13 +43,13 @@ func BenchmarkSinkFileN3(b *testing.B) {
 
 	l, err := New(SinkFile, "/tmp/")
 
-	if err != nil { 
+	if err != nil {
 		b.Fatalf("Couldn't initialize logger. Reason %v", err)
 	}
 
 	b.ResetTimer()
 
-	for i := 0 ; i < N3; i++ {
+	for i := 0; i < N3; i++ {
 		l.Info("Such meaningful %d ith message, wow.", i)
 	}
 }
