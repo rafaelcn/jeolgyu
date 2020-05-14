e4efb551acc60311bf204c799cb2f34b13d176e0
Rafael Campos Nunes <rafaelnunes@engineer.com>
2020-05-14 14:36:51 -0300

    Fix benchmarks to use available sinks
    Signed-off-by: Rafael Campos Nunes <rafaelnunes@engineer.com>

--- jeolgyu_bench_test.go
+++ jeolgyu_bench_test.go
@@ -9,8 +9,36 @@ var (
 	temp = os.TempDir()
 )
 
-func BenchmarkSinkFileN(b *testing.B) {
+func BenchmarkSinkFile(b *testing.B) {
 	l, err := New(SinkFile, temp)
+
+	if err != nil {
+		b.Fatalf("Couldn't initialize logger. Reason %v", err)
+	}
+
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		l.Info("Such meaningful %d ith message, wow.", i)
+	}
+}
+
+func BenchmarkSinkOutput(b *testing.B) {
+	l, err := New(SinkOutput, "")
+
+	if err != nil {
+		b.Fatalf("Couldn't initialize logger. Reason %v", err)
+	}
+
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		l.Info("Such meaningful %d ith message, wow.", i)
+	}
+}
+
+func BenchmarkSinkBoth(b *testing.B) {
+	l, err := New(SinkBoth, temp)
 
 	if err != nil {
 		b.Fatalf("Couldn't initialize logger. Reason %v", err)
