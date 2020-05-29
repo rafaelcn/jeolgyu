82fadb7d9634bc0322a87310c7c76b5bb05a7966
Rafael Campos Nunes <rafaelnunes@engineer.com>
2020-05-29 01:00:39 -0300

    Fixed rebasse issues (2)
        Oh boy, I did some bad things on the last rebase.
    
    Signed-off-by: Rafael Campos Nunes <rafaelnunes@engineer.com>

--- jeolgyu_bench_test.go
+++ jeolgyu_bench_test.go
@@ -22,3 +22,31 @@ func BenchmarkSinkFile(b *testing.B) {
 		l.Info("Such meaningful %d ith message, wow.", i)
 	}
 }
+
+func BenchmarkSinkOutput(b *testing.B) {
+	l, err := New(Settings{SinkType: SinkOutput})
+
+	if err != nil {
+		b.Fatalf("Couldn't initialize logger. Reason %v", err)
+	}
+
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		l.Info("Such meaningful %d ith message, wow.", i)
+	}
+}
+
+func BenchmarkSinkBoth(b *testing.B) {
+	l, err := New(Settings{SinkType: SinkFile, Filepath: temp})
+
+	if err != nil {
+		b.Fatalf("Couldn't initialize logger. Reason %v", err)
+	}
+
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		l.Info("Such meaningful %d ith message, wow.", i)
+	}
+}
